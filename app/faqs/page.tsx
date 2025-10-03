9eb1957d70522ddfa1211424a972e118bacb5182
德华 刘 <gavinprior5@gmail.com>
2025-10-03 13:18:49 +0000

    add cta to faqs

--- app/faqs/page.tsx
+++ app/faqs/page.tsx
@@ -1,10 +1,21 @@
 import { Faq8 } from "@/components/faqs/faqs";
+import { Cta10 } from "@/components/ui/cta";
 import React from "react";
 
 export default function page() {
   return (
-    <div className="max-w-9/10 mx-auto lg:min-w-4/5 flex flex-col items-center justify-evenly">
+    <div className="max-w-9/10 mx-auto lg:min-w-[90%] flex flex-col items-center justify-evenly">
       <Faq8 />
+      <Cta10
+        heading="Still Have Questions?"
+        description="Get in touch with our team for personalized assistance with your roofing needs."
+        buttons={{
+          primary: {
+            text: "Contact Us",
+            url: "/contact"
+          }
+        }}
+      />
     </div>
   );
 }
